5cca49b7994f51decc70c819739452e3526ccf6f
Unknown <rickybroers.rb@gmail.com>
2018-11-01 23:10:03 +0100

    new files

---
+++ aStar/CheckedSquare.js
@@ -0,0 +1,15 @@
+class CheckedSquare {
+
+    constructor(parentNode, locationVector) {
+        this.parentNode = parentNode;
+        this.locationVector = locationVector;
+    }
+
+    getParentNode(){
+        return this.parentNode;
+    }
+
+    getLocationVector(){
+        return this.locationVector;
+    }
+}
---
+++ aStar/PathFinder.js
@@ -0,0 +1,16 @@
+class PathFinder{
+
+    constructor(startNode, endNode) {
+        this.startNode = startNode;
+        this.endNode = endNode;
+        this.openList = [];
+        this.closedList = [];
+    }
+
+    findPath(){
+
+        //FIXME
+        console.log("path finding started");
+
+    }
+}
