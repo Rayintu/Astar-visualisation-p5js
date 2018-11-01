3e022a604f1e20c4799487437609b7d7a7bf7a36
Unknown <rickybroers.rb@gmail.com>
2018-11-01 23:35:23 +0100

    Nothing done i'm just done coding for today

--- aStar/PathFinder.js
+++ aStar/PathFinder.js
@@ -1,16 +1,24 @@
 class PathFinder{
 
     constructor(startNode, endNode) {
-        this.startNode = startNode;
+        this.currentNode = startNode;
         this.endNode = endNode;
         this.openList = [];
         this.closedList = [];
+        this.pathFound = false;
     }
 
     findPath(){
 
         //FIXME
         console.log("path finding started");
+        while(!this.pathFound){
+            this.checkSurroundingNoDiagonal();
+        }
 
+    }
+
+    checkSurroundingNoDiagonal(){
+        playField[this.currentNode.x][this.currentNode.y].getState();
     }
 }
